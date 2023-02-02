3f4b51d1061f2788836b486e6a4be04e77adce12
Wollkopf03 <69468067+Wollkopf03@users.noreply.github.com>
2023-02-02 13:27:19 +0100

    v0.10.0

--- src/user/components/Category.tsx
+++ src/user/components/Category.tsx
@@ -2,14 +2,15 @@ import { Grid, Typography } from '@mui/material'
 import { Entry, EntryType } from './Entry'
 
 type Props = {
+	id: string,
 	name: string,
 	entries: EntryType[]
 }
 
-export function Category({ name, entries }: Props) {
+export function Category({ id, name, entries }: Props) {
 	return (
 		<>
-			<Typography variant='h5' sx={{ my: 1 }}>{name}</Typography>
+			<Typography variant='h5' sx={{ my: 1 }} id={id}>{name}</Typography>
 			<Grid container spacing={3}>
 				{entries.sort((a, b) => a.name.localeCompare(b.name)).map((entry, key) => {
 					return <Entry key={key} {...entry} />
--- src/user/components/Entry.tsx
+++ src/user/components/Entry.tsx
@@ -1,4 +1,4 @@
-import { Grid, Paper, Typography } from '@mui/material'
+import { Grid, Link, Paper, Typography } from '@mui/material'
 
 export type EntryType = {
 	category: string,
@@ -13,9 +13,9 @@ export const Entry = (entry: EntryType) =>
 			{entry.information.map((information, key) =>
 				<Typography variant='body1' key={key}>
 					{information.toLowerCase().match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/) ?
-						<a href={"mailto:" + information}>{information}</a>
+						<Link href={"mailto:" + information}>{information}</Link>
 						: information.toLowerCase().match(/^(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})$/) ?
-							<a href={information.startsWith("https://") || information.startsWith("http://") ? information : "https://" + information}>{information}</a>
+							<Link href={information.startsWith("https://") || information.startsWith("http://") ? information : "https://" + information}>{information}</Link>
 							: information}
 					<br />
 				</Typography>)}
